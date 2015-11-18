2d7b1d7857eb5aeab8cf533682fb3882c01c3260
discoliam <discoliam@googlemail.com>
2015-11-18 15:05:55 +1000

    Inital Commit

---
+++ config.rb
@@ -0,0 +1,30 @@
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+activate :automatic_image_sizes
+
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+
+activate :directory_indexes
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  # activate :minify_css
+
+  # Minify Javascript on build
+  activate :minify_javascript
+
+  # Enable cache buster
+  activate :asset_hash
+
+  # Use relative URLs
+  activate :relative_assets
+
+end
---
+++ source/javascripts/application.js
@@ -0,0 +1,59 @@
+//= require "jquery"
+
+$( document ).ready(function() {
+
+
+  var menuButton = $('a.menu'),
+      mainNav = $('.main-nav'),
+      body = $('body');
+
+  // Menu Click
+  menuButton.click(function(e) {
+    e.preventDefault();
+
+    if (body.hasClass("menu-open")){
+      mainNav.fadeOut();
+      body.removeClass("menu-open");
+    } else {
+      mainNav.fadeIn();
+      body.addClass("menu-open");
+    }
+  });
+
+}); //eo:doc ready
+
+
+// Google Maps
+function initMap() {
+    var venue = {lat: 51.386205, lng: -2.362845};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: venue,
+      scrollwheel: false,
+      mapTypeControl: false,
+      streetViewControl: false,
+    });
+
+    // var image = '/images/marker.png';
+
+    var image = {
+      url: '/images/marker.png',
+      size: new google.maps.Size(66, 45),
+      scaledSize: new google.maps.Size(66, 45),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(33, 45)
+    };
+
+
+    var marker = new google.maps.Marker({
+      position: venue,
+      map: map,
+      animation: google.maps.Animation.DROP,
+      icon: image
+    });
+
+    google.maps.event.addDomListener(window, 'resize', function() {
+      map.setCenter(venue);
+    });
+  }
